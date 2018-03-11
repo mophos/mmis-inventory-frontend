b55b093a44b012dacedb21b23dd2e83d7013ed12
naphattharawat <naphattharawat@gmail.com>
2018-03-12 01:49:16 +0700

    แก้ไข stock card

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,11 +2,7 @@ export const environment = {
   production: false,
   apiUrl: 'http://localhost:3015',
   docUrl: 'http://203.157.156.69/api/document',
-<<<<<<< HEAD
-  loginUrl: 'http://localhost:3020',
-=======
   loginUrl: 'http://localhost:3004',
->>>>>>> b8e4d16932546e78f32aefd4f1163bd2defb44c6
   homeUrl: '/',
   portalUrl: 'http://203.157.156.69/api/portal',
   receivePrefix: 'REV',
