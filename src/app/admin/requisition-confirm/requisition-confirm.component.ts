d20fc8fa66475964b8705377fbe9c0c38382f1ef
Satit Rianpit <siteslave@msn.com>
2018-03-20 14:04:32 +0700

    แก้ไขคำนวณปรับยอดเบิกจากการยืมใหม่

--- src/app/admin/requisition-confirm/requisition-confirm.component.ts
+++ src/app/admin/requisition-confirm/requisition-confirm.component.ts
@@ -382,7 +382,11 @@ export class RequisitionConfirmComponent implements OnInit {
           let idx = _.findIndex(data, { generic_id: v.generic_id });
           if (idx > -1) {
             data[idx].qty += v.qty * v.conversion_qty;
-          } else data.push(v);
+          } else {
+            let obj: any = v;
+            obj.qty = v.qty * v.conversion_qty;
+            data.push(obj);
+          }
         });
 
         this.products.forEach((x, i) => {
@@ -392,7 +396,7 @@ export class RequisitionConfirmComponent implements OnInit {
             let pQty = this.products[i].requisition_qty * this.products[i].conversion_qty;
             let reqQty = pQty + data[idx].qty;
             // จำนวนจ่ายจริง
-            this.products[i].requisition_qty = Math.floor(reqQty / this.products[i].conversion_qty);
+            // this.products[i].requisition_qty = Math.floor(reqQty / this.products[i].conversion_qty);
             // จำนวนที่ยืมไป
             dataBorrow.push({
               // borrowNoteDetailId: data[idx].borrow_note_detail_id,
@@ -429,13 +433,15 @@ export class RequisitionConfirmComponent implements OnInit {
 
           if (rs.ok) {
             this.alertService.success();
-            await this.getOrderItems();
           } else {
             this.alertService.error(rs.error);
             this.selectedBorrowNotes = [];
             this.borrowNotes = [];
-            await this.getBorrowNotes();
           }
+          
+          await this.getOrderItems();
+          await this.getBorrowNotes();
+
           this.openBorrowNote = false;
         } catch (error) {
           console.log(error);
