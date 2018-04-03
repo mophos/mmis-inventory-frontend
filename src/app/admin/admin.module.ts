a656506455f8d826dfe2811e1a051634d76cee2f
peemppk <peemppk5538@gmail.com>
2018-04-03 09:20:39 +0700

    รายงานการจัดซื้อ

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -138,6 +138,7 @@ import { BorrowNoteNewComponent } from './borrow-note-new/borrow-note-new.compon
 import { BorrowNoteService } from './borrow-note.service';
 import { RequisitionConfirmEditComponent } from './requisition-confirm-edit/requisition-confirm-edit.component';
 import { CalculateMinMaxComponent } from 'app/admin/calculate-min-max/calculate-min-max.component';
+import { PurchasingNotgiveawayComponent } from './reports/purchasing-notgiveaway/purchasing-notgiveaway.component';
 
 export function highchartsFactory() {
   return require('highcharts');
@@ -230,7 +231,8 @@ Highcharts.setOptions({
     BorrowNoteComponent,
     BorrowNoteNewComponent,
     RequisitionConfirmEditComponent,
-    CalculateMinMaxComponent
+    CalculateMinMaxComponent,
+    PurchasingNotgiveawayComponent
   ],
   providers: [
     MainService,
