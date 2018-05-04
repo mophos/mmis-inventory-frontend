1fd747dcf8308906201b3e5049a79f8c73608a77
peemppk <peemppk5538@gmail.com>
2018-05-04 17:19:01 +0700

    checkpoint

--- src/app/admin/reports/product-remain/product-remain.component.ts
+++ src/app/admin/reports/product-remain/product-remain.component.ts
@@ -7,6 +7,9 @@ import {
   ChangeDetectorRef,
   Inject
 } from '@angular/core';
+import { tokenNotExpired, JwtHelper } from 'angular2-jwt';
+import { ProductsService } from '../../products.service';
+
 
 import * as moment from 'moment';
 import * as _ from 'lodash';
@@ -20,7 +23,7 @@ export class ProductRemainComponent implements OnInit {
   products: any = [];
   allWarehouseProducts = [];
   warehouses: any = [];
-  warehouseId: number;
+  warehouseId: any;
   openGraph = false;
   openAllWarehouse = false;
   loading = false;
@@ -29,151 +32,49 @@ export class ProductRemainComponent implements OnInit {
   demoOption: any;
   chartOptions: any;
   token: string;
+  genericTypes = [];
+  genericType: any = 0;
+  genericTypeIds = [];
+
+  jwtHelper: JwtHelper = new JwtHelper();
 
   constructor(
     private warehouseService: WarehouseService,
     private reportProductService: ReportProductsService,
+    private productService: ProductsService,
     private alertService: AlertService,
     private ref: ChangeDetectorRef,
     @Inject('API_URL') private url: string
   ) {
     this.token = sessionStorage.getItem('token');
+    const decodedToken = this.jwtHelper.decodeToken(this.token);
+    this.genericTypeIds = decodedToken.generic_type_id ? decodedToken.generic_type_id.split(',') : [];
+    this.warehouseId = decodedToken.warehouseId;
   }
 
   ngOnInit() {
-    this.getWarehouseList();
+    this.getGenericType();
   }
 
-  getRemain() {
-    this.loading = false;
-    this.products = [];
-    this.reportProductService.getProductRemain(this.warehouseId)
-      .then((result: any) => {
-        this.loading = false;
-        this.products = result.rows;
-        this.setChartData();
-        this.ref.detectChanges();
-      })
-      .catch(error => {
-        this.loading = false;
-        this.alertService.error(error.message);
-      });
-  }
-
-  getRemainAllWarehouse(product) {
-    this.allWarehouseProducts = [];
-    this.reportProductService.getProductRemainAllWarehouse(product.product_id)
-      .then((result: any) => {
-        if (result.ok) {
-          this.allWarehouseProducts = result.rows;
-          this.openAllWarehouse = true;
-        } else {
-          this.alertService.error(JSON.stringify(result.error));
-        }
-      })
-      .catch(error => {
-        this.alertService.serverError();
-      });
-  }
-
-  setChartData() {
-    const chartData = [];
-    const categories = [];
-
-    const _products = _.orderBy(this.products, ['total'], ['desc']);
-    _.forEach(_products, (v: any) => {
-      categories.push(v.product_name);
-      chartData.push(v.total);
-    });
-
-    console.log(_products);
-
-    this.demoOption = {
-      chart: {
-        type: 'column'
-      },
-      title: { text: 'สถิติการรับสินค้าเข้าคลัง' },
-      xAxis: {
-        categories: categories
-      },
-      plotOptions: {
-        line: {
-          dataLabels: {
-            enabled: true
-          },
-          enableMouseTracking: false
-        }
-      },
-      series: [{
-        name: 'ปริมาณการรับเข้า',
-        data: chartData,
-      }]
+  async getGenericType() {
+    try {
+      const rs = await this.productService.getGenericType();
+      if (rs.ok) {
+        this.genericTypes = rs.rows;
+      } else {
+        this.alertService.error(rs.error);
+      }
+    } catch (error) {
+      console.log(error);
+      this.alertService.serverError();
     }
   }
 
-  getWarehouseList() {
-    this.warehouseService.all()
-      .then((result: any) => {
-        if (result.ok) {
-          this.warehouses = result.rows;
-        } else {
-          this.alertService.error(JSON.stringify(result.error));
-        }
-      });
+  changeGenericType() {
+    console.log(this.genericType, 'genericType');
   }
 
-  pdfExport() {
-    if (this.warehouseId) {
-      const url = `${this.url}/reports/products/remain/pdf/${this.warehouseId}?token=${this.token}`;
-      window.open(url);
-    } else {
-      this.alertService.error('กรุณาระบุคลังสินค้า');
-    }
-  }
-
-  excelExport() {
-    if (this.warehouseId) {
-      const url = `${this.url}/reports/products/remain/excel/${this.warehouseId}?token=${this.token}`;
-      window.open(url);
-    } else {
-      this.alertService.error('กรุณาระบุคลังสินค้า');
-    }
-  }
-
-  showGraph(productId: string) {
-    this.productId = productId;
-
-    this.reportProductService.getProductReceives(productId)
-      .then((result: any) => {
-        const categories = [];
-        const _data = [];
-        const rows = <Array<any>>result.rows;
-        rows.forEach(v => {
-          const date = `${moment(v.receive_date).format('DD/MM')}/${moment(v.receive_date).get('year') + 543}`;
-          categories.push(date);
-          _data.push(+v.total);
-        });
-
-        this.chartOptions = {
-          title: { text: 'สถิติการรับสินค้าเข้าคลัง' },
-          xAxis: {
-            categories: categories
-          },
-          plotOptions: {
-            line: {
-              dataLabels: {
-                enabled: true
-              },
-              enableMouseTracking: false
-            }
-          },
-          series: [{
-            name: 'ปริมาณการรับเข้า',
-            data: _data,
-          }]
-        };
-
-        this.openGraph = true;
-      });
+  setSelectedWarehouse(event) {
+    this.warehouseId = event.warehouse_id;
   }
 }
