ebb08671bb87438d0c9851098667abbf8b59075c
naphattharawat <naphattharawat@gmail.com>
2018-07-09 23:39:06 +0700

    Update product-requisition.component.ts

--- src/app/staff/product-requisition/product-requisition.component.ts
+++ src/app/staff/product-requisition/product-requisition.component.ts
@@ -105,7 +105,6 @@ export class ProductRequisitionComponent implements OnInit {
       }
       if (result.ok) {
         this.generics = result.rows;
-        console.log(this.generics)
       } else {
         this.alertService.error('เกิดข้อผิดพลาด: ' + JSON.stringify(result.error));
       }
