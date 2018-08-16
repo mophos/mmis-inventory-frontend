fd2a3be3fcd1b3f601f997fb6bdef8cab0752224
Peem <peemppk5538@gmail.com>
2018-08-16 16:24:29 +0700

    รายงานสถานะเวชภัณฑ์คงคลัง excel

--- src/app/admin/reports/inventory-status/inventory-status.component.ts
+++ src/app/admin/reports/inventory-status/inventory-status.component.ts
@@ -8,6 +8,8 @@ import { AlertService } from './../../../alert.service';
 import * as _ from 'lodash';
 import { ProductsService } from '../../products.service';
 import { State } from "@clr/angular";
+import { WarehouseService } from './../../warehouse.service';
+
 
 @Component({
   selector: 'wm-inventory-status',
@@ -23,8 +25,10 @@ export class InventoryStatusComponent implements OnInit {
   warehouseId: any;
   genericTypes = [];
   genericType: any = 0;
+  warehouses: any = [];
   genericTypeIds = [];
   genericTypeSelect: any = [];
+  warehouseName: any;
   myDatePickerOptions: IMyOptions = {
     inline: false,
     dateFormat: 'dd mmm yyyy',
@@ -38,12 +42,14 @@ export class InventoryStatusComponent implements OnInit {
   constructor(
     private alertService: AlertService,
     private productService: ProductsService,
+    private warehouseService: WarehouseService,
     @Inject('API_URL') private apiUrl: string
   ) {
     this.token = sessionStorage.getItem('token');
     const decodedToken = this.jwtHelper.decodeToken(this.token);
     this.genericTypeIds = decodedToken.generic_type_id ? decodedToken.generic_type_id.split(',') : [];
     this.warehouseId = decodedToken.warehouseId;
+    this.warehouseName = decodedToken.warehouseName
   }
 
 
@@ -57,10 +63,18 @@ export class InventoryStatusComponent implements OnInit {
       }
     };
     this.getGenericType();
+    this.getWarehouseList();
   }
 
-  setSelectedWarehouse(event) {
-    this.warehouseId = event.warehouse_id;
+  getWarehouseList() {
+    this.warehouseService.all()
+      .then((result: any) => {
+        if (result.ok) {
+          this.warehouses = result.rows;
+        } else {
+          this.alertService.error(JSON.stringify(result.error));
+        }
+      });
   }
 
   async getGenericType() {
@@ -81,16 +95,39 @@ export class InventoryStatusComponent implements OnInit {
   }
 
   showReport() {
+    if (+this.warehouseId !== 0) {
+      this.warehouseName = _.find(this.warehouses, (v) => { return +v.warehouse_id === +this.warehouseId })
+      this.warehouseName = this.warehouseName.warehouse_name
+    } else {
+      this.warehouseName = 'ทุกคลังสินค้า'
+    }
+    console.log(this.warehouseId);
     let genericType = [];
     this.genericTypeSelect.forEach(value => {
       genericType.push('genericType=' + value.generic_type_id)
     });
     this.statusDate = this.statusDate ? moment(this.statusDate.jsdate).format('YYYY-MM-DD') : null;
-    const url = `${this.apiUrl}/report/inventorystatus?warehouseId=${this.warehouseId}&statusDate=${this.statusDate}&token=${this.token}&` + genericType.join('&');
+    const url = `${this.apiUrl}/report/inventorystatus?warehouseId=${this.warehouseId}&statusDate=${this.statusDate}&warehouseName=${this.warehouseName}&token=${this.token}&` + genericType.join('&');
     this.htmlPreview.showReport(url);
   }
-  
+
   refreshWaiting(state: State) {
     this.getGenericType();
+  }
+
+  exportExcel() {
+    if (+this.warehouseId !== 0) {
+      this.warehouseName = _.find(this.warehouses, (v) => { return +v.warehouse_id === +this.warehouseId })
+      this.warehouseName = this.warehouseName.warehouse_name
+    } else {
+      this.warehouseName = 'ทุกคลังสินค้า'
+    }
+    let genericType = [];
+    this.genericTypeSelect.forEach(value => {
+      genericType.push('genericType=' + value.generic_type_id)
+    });
+    this.statusDate = this.statusDate ? moment(this.statusDate.jsdate).format('YYYY-MM-DD') : null;
+    const url = `${this.apiUrl}/report/inventorystatus/excel?warehouseId=${this.warehouseId}&statusDate=${this.statusDate}&warehouseName=${this.warehouseName}&token=${this.token}&` + genericType.join('&');
+    window.open(url, '_blank');
   }
 }
