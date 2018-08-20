075acbfcd867d1d6cf8b03544fe109bccc706953
Peem <peemppk5538@gmail.com>
2018-08-20 21:54:46 +0700

    checkpoint

---
+++ src/app/staff/report/value-receive-other/value-receive-other.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'wm-value-receive-other',
+  templateUrl: './value-receive-other.component.html',
+  styles: []
+})
+export class ValueReceiveOtherComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
