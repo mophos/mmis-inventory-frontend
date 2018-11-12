618cce9ab467e4ecc4829800962ea428c772a63f
naphattharawat <naphattharawat@gmail.com>
2018-11-12 12:58:38 +0700

    แก้ การปรับราคา ของยาในคลังสินค้า

--- src/app/directives/search-generic-warehouse-autocomplete/search-generic-warehouse-autocomplete.component.ts
+++ src/app/directives/search-generic-warehouse-autocomplete/search-generic-warehouse-autocomplete.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, Inject, Output, EventEmitter, Input } from '@angular/core';
-import { JwtHelper } from 'angular2-jwt';
 
 @Component({
   selector: 'wm-search-generic-warehouse-autocomplete',
@@ -9,18 +8,18 @@ export class SearchGenericWarehouseAutocompleteComponent implements OnInit {
   @Output('onSelect') onSelect: EventEmitter<any> = new EventEmitter<any>();
   @Output('onChange') onChange: EventEmitter<any> = new EventEmitter<any>();
   @Input('clearOnSelected') clearOnSelected: EventEmitter<boolean> = new EventEmitter<boolean>();
-  
+
   @Input('disabled')
   set setDisabled(value: boolean) {
     this._disabled = value;
   }
-  
+
   @Input('warehouseId')
   set setWarehouseId(value: any) {
     this.setApiUrl(value);
-  } 
-  
-  _disabled: boolean = false;
+  }
+
+  _disabled = false;
   _warehouseId: any;
 
   token: any;
@@ -45,7 +44,7 @@ export class SearchGenericWarehouseAutocompleteComponent implements OnInit {
   }
 
   clearSelected(event: any) {
-    if (this.clearOnSelected) {
+    if (this.clearOnSelected || (event.keyCode === 80 && this.query === '')) {
       if (event.keyCode === 13 || event.keyCode === 37 || event.keyCode === 38 || event.keyCode === 39 || event.keyCode === 40) {
         this.onChange.emit(true);
       } else {
