b008128df055bc50258ce3e2d615926bd1e8f048
Peem <peemppk5538@gmail.com>
2019-01-03 16:10:52 +0700

    genericType in report  receive

--- src/app/admin/reports/product-receive/product-receive.component.ts
+++ src/app/admin/reports/product-receive/product-receive.component.ts
@@ -3,6 +3,7 @@ import { IMyOptions } from 'mydatepicker-th';
 import { AlertService } from './../../../alert.service';
 import * as moment from 'moment';
 import { tokenNotExpired, JwtHelper } from 'angular2-jwt';
+import { ProductsService } from '../../products.service';
 
 @Component({
   selector: 'wm-product-receive',
@@ -23,15 +24,20 @@ export class ProductReceiveComponent implements OnInit {
     editableDateField: false,
     showClearDateBtn: false
   };
+  genericTypes = [];
+  genericTypeSelect: any = [];
 
   constructor(
-    @Inject('API_URL') private apiUrl: string
+    @Inject('API_URL') private apiUrl: string,
+    private productService: ProductsService,
+    private alertService: AlertService
   ) {
     this.token = sessionStorage.getItem('token');
     const decodedToken = this.jwtHelper.decodeToken(this.token);
   }
 
   ngOnInit() {
+    this.getGenericsType();
     const date = new Date();
 
     this.startDate = {
@@ -50,17 +56,39 @@ export class ProductReceiveComponent implements OnInit {
     };
   }
 
+  async getGenericsType() {
+    try {
+      const rs = await this.productService.getGenericType();
+      if (rs.ok) {
+        this.genericTypes = rs.rows;
+      } else {
+        this.alertService.error(rs.error);
+      }
+    } catch (error) {
+      console.log(error);
+      this.alertService.serverError();
+    }
+  }
+
   ptintReport() {
+    let genericType = [];
+    this.genericTypeSelect.forEach(value => {
+      genericType.push('genericType=' + value.generic_type_id)
+    });
     this.start = this.startDate ? `${this.startDate.date.year}-${this.startDate.date.month}-${this.startDate.date.day}` : null;
     this.end = this.endDate ? `${this.endDate.date.year}-${this.endDate.date.month}-${this.endDate.date.day}` : null;
-    const url = `${this.apiUrl}/report/product-receive?startDate=${this.start}&endDate=${this.end}&token=${this.token}`
+    const url = `${this.apiUrl}/report/product-receive?startDate=${this.start}&endDate=${this.end}&token=${this.token}&` + genericType.join('&');
     this.htmlPreview.showReport(url, 'landscape');
   }
 
   exportExcel() {
+    let genericType = [];
+    this.genericTypeSelect.forEach(value => {
+      genericType.push('genericType=' + value.generic_type_id)
+    });
     this.start = this.startDate ? `${this.startDate.date.year}-${this.startDate.date.month}-${this.startDate.date.day}` : null;
     this.end = this.endDate ? `${this.endDate.date.year}-${this.endDate.date.month}-${this.endDate.date.day}` : null;
-    const url = `${this.apiUrl}/report/receive/export?startDate=${this.start}&endDate=${this.end}&token=${this.token}`
+    const url = `${this.apiUrl}/report/receive/export?startDate=${this.start}&endDate=${this.end}&token=${this.token}&` + genericType.join('&');
     window.open(url, '_blank');
   }
 
