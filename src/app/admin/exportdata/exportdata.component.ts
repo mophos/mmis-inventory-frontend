951f4a373031e6dbe5c0494f9c8a9af80945fc4b
naphattharawat <naphattharawat@gmail.com>
2019-07-18 14:00:01 +0700

    export trace

--- src/app/admin/exportdata/exportdata.component.ts
+++ src/app/admin/exportdata/exportdata.component.ts
@@ -17,15 +17,39 @@ export class ExportdataComponent implements OnInit {
   ngOnInit() {
   }
 
-  async exportRemain() {
+  exportRemain() {
     const token = sessionStorage.getItem('token');
     const url = `${this.apiUrl}/report/remain/qty/export?token=${token}`;
     window.open(url, '_blank');
   }
 
-  async exportRemainTrade() {
+  exportRemainTrade() {
     const token = sessionStorage.getItem('token');
     const url = `${this.apiUrl}/report/remain-trade/qty/export?token=${token}`;
+    window.open(url, '_blank');
+  }
+
+  exportDistribute() {
+    const token = sessionStorage.getItem('token');
+    const url = `${this.apiUrl}/reports/export/distribute?token=${token}`;
+    window.open(url, '_blank');
+  }
+
+  exportDruglist() {
+    const token = sessionStorage.getItem('token');
+    const url = `${this.apiUrl}/reports/export/druglist?token=${token}`;
+    window.open(url, '_blank');
+  }
+
+  exportInventory() {
+    const token = sessionStorage.getItem('token');
+    const url = `${this.apiUrl}/reports/export/inventory?token=${token}`;
+    window.open(url, '_blank');
+  }
+
+  exportReceive() {
+    const token = sessionStorage.getItem('token');
+    const url = `${this.apiUrl}/reports/export/receive?token=${token}`;
     window.open(url, '_blank');
   }
 }
